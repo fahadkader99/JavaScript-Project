7c81205ba095ff1ef4a08477639c673074c0c5fa
Fahad Kader <fahadkader786@outlook.com>
2024-06-02 13:35:11 -0400

    All code based updated for the game

--- project.js
+++ project.js
@@ -7,62 +7,163 @@
 // 7. Play again or not ?
 /*--------------------------------------------------------------*/
 
+// Global variables
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
+
+const SYMBOL_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 1,
+};
 
 // 1. Function - to deposite $$$
 const deposit = () => {
-    while(true){
-        const depositAmount = prompt("\nEnter a deposit amount: ");
-        const numberDepositAmount = parseFloat(depositAmount);
-    
-        if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
-            console.log("\nInvalid deposit amount, try again !");
-        }else{
-            return numberDepositAmount;
-        }
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again !");
+    } else {
+      return numberDepositAmount;
     }
-
+  }
 };
-
 
 // 2. Function - number of lines to bet on
 const getNumberOfLines = () => {
-    while(true){
-        const lines = prompt("\nEnter the number of lines to bet on (1-3): ");
-        const numberOfLines = parseFloat(lines);
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
 
-        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
-            console.log("\nInvalid number of lines, try again !");
-        }else{
-            return numberOfLines;
-        }
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again !");
+    } else {
+      return numberOfLines;
     }
-}
-
+  }
+};
 
 // 3. Function - Collect a bet amount
-const getBet = (balance) => {
-    while(true){
-        const bet = prompt("\nEnter total bet: ");
-        const numberBet = parseFloat(bet);
-    
-        if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance){
-            console.log("Invalid bet, try again !");
-        }else{
-            return numberBet;
-        }
+const getBet = (balance, lines) => {
+  while (true) {
+    const bet = prompt("Enter total bet per line: ");
+    const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+      console.log("Invalid bet, try again !");
+    } else {
+      return numberBet;
     }
-}
+  }
+};
 
+// 4. Function - Spin the slot machine
+const spin = () => {
+  const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
 
-//let balance = deposit();
-//console.log(balance);
+  const reels = [];
+  for (let i = 0; i < COLS; i++) {
+    reels.push([]);
+    const reelSymbols = [...symbols];
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
+    }
+  }
+  return reels;
+};
 
-// const numberOfLines = getNumberOfLines();
-// console.log(numberOfLines);
+const transpose = (reels) => {
+  const rows = [];
 
-const betAmount = getBet();
-console.log(betAmount);
+  for (let i = 0; i < ROWS; i++) {
+    rows.push([]);
+    for (let j = 0; j < COLS; j++) {
+      rows[i].push(reels[j][i]);
+    }
+  }
+  return rows;
+};
 
+const printRows = (rows) => {
+  for (const row of rows) {
+    let rowString = "";
+    for (const [i, symbol] of row.entries()) {
+      rowString += symbol;
+      if (i != row.length - 1) {
+        rowString += " | ";
+      }
+    }
+    console.log(rowString);
+  }
+};
 
+// 5. Function - Check winnings
+const getWinnings = (rows, bet, lines) => {
+  let winnings = 0;
+
+  for (let row = 0; row < lines; row++) {
+    const symbols = rows[row];
+    let allSame = true;
+
+    for (const symbol of symbols) {
+      if (symbol != symbols[0]) {
+        allSame = false;
+        break;
+      }
+    }
+
+    if (allSame) {
+      winnings += bet * SYMBOL_VALUES[symbols[0]];
+    }
+  }
+  return winnings;
+};
+
+// Function - Game function contains all core logic
+const game = () => {
+  let balance = deposit();
+
+  while (true) {
+    console.log("You have a balance of $" + balance);
+    const numberOfLines = getNumberOfLines();
+    const bet = getBet(balance, numberOfLines);
+    balance -= bet * numberOfLines;
+    const reels = spin();
+    const rows = transpose(reels);
+    printRows(rows);
+    const winnings = getWinnings(rows, bet, numberOfLines);
+    balance += winnings;
+    console.log("You won, $" + winnings.toString());
+
+    if (balance <= 0) {
+      console.log("You ran out of money!");
+      break;
+    }
+
+    const playAgain = prompt("Do you want to play again (y/n)? ");
+
+    if (playAgain != "y") break;
+  }
+};
+
+game();
